a9bd18b42ccf38546ca0706fde68712273c8e946
ibrahim20088 <87567802+ibrahim20088@users.noreply.github.com>
2023-06-24 12:07:21 +0300

    Update code.js

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`  July 4 , 2023 9:00:00 
+const newYearTime = new Date(`  July 21 , 2023 9:00:00 
 `);
 
 // Update countdown time
